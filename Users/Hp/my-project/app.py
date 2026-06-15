f1070499c6935314907dc87cb3cda5043cb0b55c
devsecops <devsecops@local.com>
2026-06-15 12:46:26 +0530

    merge: health response from both branches

---
+++ Users/Hp/my-project/app.py
@@ -0,0 +1,14 @@
+from flask import flask, jsonify
+app= FLSK(__name__)
+@app.route('/health')
+def health();
+return jsonify({"status"; "ok","version":"1.0","uptime":999})
+if __name__=='__main__';
+app.run(debug=true)
+@app.route('/version')
+def version();
+rerturn jsonify({"version": "1.0.0"})
+@app.route('/login' , methods={'POST'})
+def login ():
+return {"message": "login incorrect"}
+
