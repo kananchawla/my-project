7402ee49f4235701f67eac141ef4818e5940e530
kananchawla <kanan.chawla.kc@gmail.com>
2026-07-06 11:34:12 +0530

    fix: added root route to flask app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['POST'])
-def login():
-    return '{"message": "login endpoint"}'
+@app.route('/')
+def home():
+    return "Hello, DevSecOps! The server is fully functional!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
