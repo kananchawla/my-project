0909c2f82459f4a498bc2ab07dafdff1753641c6
kananchawla <kanan.chawla.kc@gmail.com>
2026-07-06 11:39:30 +0530

    ci: add flask health check pipeline

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def home():
     return "Hello, DevSecOps! The server is fully functional!"
 
+
+@app.route('/health')
+    return jsonify({
+        "uptime": 999,
+        "version": "1.0"
+    })
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
