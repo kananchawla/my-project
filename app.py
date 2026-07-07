c4a3202c115ade2d3edaa1a0f8e7335311c02e15
kananchawla <kanan.chawla.kc@gmail.com>
2026-07-07 11:03:42 +0530

    fix: resolve indentation and add debug=False

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-
-
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "Hello, DevSecOps! The server is fully functional!"
 
 
-@app.route('/health')
-    return jsonify({
-        "uptime": 999,
-        "version": "1.0"
-    })
+@app.route("/health")
+def health():  # It is good practice to name the function
+    return jsonify({"uptime": 999, "version": "1.0"})
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=False)
